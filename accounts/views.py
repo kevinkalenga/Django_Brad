f544d59cf78ff98f4738df9d733961d807a203f0
Kalenga243 <kevinkalenga10@gmail.com>
2025-06-27 11:03:26 +0200

    User Login

--- accounts/views.py
+++ accounts/views.py
@@ -38,10 +38,20 @@ def register(request):
     else:
        return render(request, 'accounts/register.html')
 
-def login(request): 
+def login_view(request): 
     if request.method == 'POST':
-      # Login User
-      return
+      username = request.POST['username']
+      password = request.POST['password']
+
+      user = auth.authenticate(username=username, password=password)
+
+      if user is not None:
+         auth.login(request, user)
+         messages.success(request, 'You are now logged in')
+         return redirect('accounts:dashboard')
+      else:
+        messages.error(request, 'Invalid credentials')
+        return redirect('accounts:login')
     else:
        return render(request, 'accounts/login.html')
 
